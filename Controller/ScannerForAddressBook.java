ac2a4119f0e33e2187bd9a811bcec328e816e173
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-09 21:50:20 +0530

    [Vinayak]Add:Scanner class for AdressBook is Added

--- Controller/ScannerForAddressBook.java
+++ Controller/ScannerForAddressBook.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ScannerForAddressBook {
 	public Scanner scannerProvider() {
-		Scanner scanner = new Scanner(System.in);
-		return scanner;
+	  Scanner scanner = new Scanner(System.in);
+	  return scanner;
 	}
 }
